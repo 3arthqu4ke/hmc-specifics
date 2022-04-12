ccb2fb485090fce00610e94490d947e9706c721f
3arthqu4ke <hanspeteroliver.mueller@gmail.com>
2022-04-12 12:42:32 +0200

    Made the close and menu commands schedulable

--- src/main/java/me/earth/headlessmc/mc/commands/CloseCommand.java
+++ src/main/java/me/earth/headlessmc/mc/commands/CloseCommand.java
@@ -4,7 +4,8 @@ import me.earth.headlessmc.api.HeadlessMc;
 import me.earth.headlessmc.mc.Minecraft;
 import me.earth.headlessmc.mc.player.Player;
 
-public class CloseCommand extends AbstractPlayerCommand {
+public class CloseCommand extends AbstractPlayerCommand
+    implements ScheduledCommand {
     public CloseCommand(HeadlessMc ctx, Minecraft mc) {
         super(ctx, mc, "close", "Closes the current screen, if ingame.");
     }
--- src/main/java/me/earth/headlessmc/mc/commands/MenuCommand.java
+++ src/main/java/me/earth/headlessmc/mc/commands/MenuCommand.java
@@ -4,7 +4,8 @@ import me.earth.headlessmc.api.HeadlessMc;
 import me.earth.headlessmc.mc.Minecraft;
 import me.earth.headlessmc.mc.player.Player;
 
-public class MenuCommand extends AbstractPlayerCommand {
+public class MenuCommand extends AbstractPlayerCommand
+    implements ScheduledCommand {
     public MenuCommand(HeadlessMc ctx, Minecraft mc) {
         super(ctx, mc, "menu", "Opens the menu if you are ingame.");
     }
