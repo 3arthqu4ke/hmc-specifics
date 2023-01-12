918c8e1a3e60332034f739583e778c3de7fc8ed4
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2023-01-12 16:58:41 +0100

    [1.12.2-b3] Updated to 1.1.0 HMC-Specifics and added Dis/Connect Commands

--- src/main/java/me/earth/headlessmc/mc/mixins/MixinMinecraft.java
+++ src/main/java/me/earth/headlessmc/mc/mixins/MixinMinecraft.java
@@ -8,7 +8,12 @@ import me.earth.headlessmc.mc.gui.FontRenderer;
 import me.earth.headlessmc.mc.gui.GuiScreen;
 import me.earth.headlessmc.mc.player.Player;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiMultiplayer;
 import net.minecraft.client.main.GameConfiguration;
+import net.minecraft.client.multiplayer.GuiConnecting;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.realms.RealmsBridge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,12 +29,22 @@ public abstract class MixinMinecraft implements Minecraft {
     public EntityPlayerSP player;
     @Shadow
     public net.minecraft.client.gui.GuiScreen currentScreen;
+    @Shadow
+    public WorldClient world;
 
     @Shadow
     public abstract void shutdown();
-
     @Shadow
     public abstract ListenableFuture<Object> addScheduledTask(Runnable run);
+    @Shadow
+    public abstract boolean isIntegratedServerRunning();
+    @Shadow
+    public abstract boolean isConnectedToRealms();
+    @Shadow
+    public abstract void loadWorld(WorldClient worldClientIn);
+    @Shadow
+    public abstract void displayGuiScreen(
+        net.minecraft.client.gui.GuiScreen guiScreenIn);
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(GameConfiguration gameConfig, CallbackInfo ci)
@@ -60,6 +75,34 @@ public abstract class MixinMinecraft implements Minecraft {
     @Override
     public Future<?> schedule(Runnable task) {
         return addScheduledTask(task);
+    }
+
+    @Override
+    public void connect(String ip, int port) {
+        this.disconnect();
+        this.displayGuiScreen(new GuiConnecting(
+            new GuiMainMenu(), net.minecraft.client.Minecraft.class.cast(this),
+            ip, port));
+    }
+
+    @Override
+    public void disconnect() {
+        boolean singlePlayer = this.isIntegratedServerRunning();
+        boolean realms = this.isConnectedToRealms();
+
+        if (world != null) {
+            world.sendQuittingDisconnectingPacket();
+        }
+
+        this.loadWorld(null);
+
+        if (singlePlayer) {
+            this.displayGuiScreen(new GuiMainMenu());
+        } else if (realms) {
+            new RealmsBridge().switchToRealms(new GuiMainMenu());
+        } else {
+            this.displayGuiScreen(new GuiMultiplayer(new GuiMainMenu()));
+        }
     }
 
 }
