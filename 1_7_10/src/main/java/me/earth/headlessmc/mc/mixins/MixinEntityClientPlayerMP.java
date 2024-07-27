15abe2a22741e751d43260f77d19263cb3595069
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-07-27 19:01:41 +0200

    Fixed 1.7.10, though the mixin refmap does not contain MixinEntityClientPlayerMP.sendQueue field?

--- 1_7_10/src/main/java/me/earth/headlessmc/mc/mixins/MixinEntityClientPlayerMP.java
+++ 1_7_10/src/main/java/me/earth/headlessmc/mc/mixins/MixinEntityClientPlayerMP.java
@@ -4,14 +4,22 @@ import me.earth.headlessmc.mc.player.Player;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.GuiIngameMenu;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.play.client.C01PacketChatMessage;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EntityClientPlayerMP.class)
-public abstract class MixinEntityPlayerSP implements Player {
+public abstract class MixinEntityClientPlayerMP implements Player {
+    @Shadow
+    @Final
+    public NetHandlerPlayClient sendQueue;
+
     @Override
-    @Invoker("sendChatMessage")
-    public abstract void sendMessage(String message);
+    public void sendMessage(String message) {
+        this.sendQueue.addToSendQueue(new C01PacketChatMessage(message));
+    }
 
     @Override
     public void openMenu() {
